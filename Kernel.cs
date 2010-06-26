ee8f9a030548ce63f7a6ebcde3e59568d11aa8b3
Stefan O'Rear <stefanor@cox.net>
2010-06-26 10:08:15 +0000

    [vicil] Expand scope of KERNEL a bit

--- Kernel.cs
+++ Kernel.cs
@@ -7,21 +7,21 @@ namespace Sprixel {
 
     // Only call other functions in Continue, not in the CallableDelegate or
     // equivalent!
-    public delegate FrameBase CallableDelegate(FrameBase caller,
+    public delegate Frame CallableDelegate(Frame caller,
             LValue pos[], Dictionary<string, LValue> named);
     // Used by DynFrame to plug in code
     public delegate void DynBlockDelegate(DynamicFrame frame);
 
     public interface IPerl6Object {
-        public FrameBase Invoke(FrameBase caller, LValue pos[],
+        public Frame Invoke(Frame caller, LValue pos[],
                 Dictionary<string, LValue> named);
         // include the invocant in the positionals!  it will not usually be
         // this, rather a container of this
-        public FrameBase InvokeMethod(FrameBase caller, string name,
+        public Frame InvokeMethod(Frame caller, string name,
                 LValue pos[], Dictionary<string, LValue> named);
-        public FrameBase GetAttribute(FrameBase caller, string name);
-        public FrameBase WHERE(FrameBase caller);
-        public FrameBase HOW(FrameBase caller);
+        public Frame GetAttribute(Frame caller, string name);
+        public Frame WHERE(Frame caller);
+        public Frame HOW(Frame caller);
     }
 
     public struct LValue {
@@ -34,45 +34,42 @@ namespace Sprixel {
         public bool bvalue;
     }
 
-    public abstract class FrameBase: IPerl6Object {
-        public readonly FrameBase caller;
-        public readonly FrameBase outer;
+    // We need hashy frames available to properly handle BEGIN; for the time
+    // being, all frames will be hashy for simplicity
+    public abstract class Frame: IPerl6Object {
+        public readonly Frame caller;
+        public readonly Frame outer;
         public object resultSlot = null;
         public int ip = 0;
-
-        public abstract FrameBase Continue();
-    }
-
-    public class DynFrame: FrameBase {
         public readonly DynBlockDelegate code;
         // very generic because it also has to hold spills.
         public readonly Dictionary<string, object> lex
             = new Dictionary<string, object>;
 
-        public DynFrame(FrameBase caller_, FrameBase outer_,
+        public DynFrame(Frame caller_, Frame outer_,
                 DynBlockDelegate code_) {
             caller = caller_;
             outer = outer_;
             code = code_;
         }
 
-        public FrameBase Continue() {
+        public Frame Continue() {
             return code(this);
         }
 
-        public FrameBase GetSlot(FrameBase c, string name) {
+        public Frame GetSlot(Frame c, string name) {
             c.resultSlot = lex[name];
         }
     }
 
-    public class ExceptionHelper: FrameBase {
-        private FrameBase cursor;
+    public class ExceptionHelper: Frame {
+        private Frame cursor;
         private LValue toThrowPos[];
         private Dictionary<string, LValue> toThrowNamed;
 
-        private ExceptionHelper(FrameBase caller_) { caller = caller_; }
+        private ExceptionHelper(Frame caller_) { caller = caller_; }
 
-        public static FrameBase Throw(FrameBase caller, LValue pos[],
+        public static Frame Throw(Frame caller, LValue pos[],
                 Dictionary<string, LValue> named) {
             var n = new ExceptionHelper();
             n.cursor = caller;
@@ -81,7 +78,7 @@ namespace Sprixel {
             return n;
         }
 
-        public FrameBase Continue() {
+        public Frame Continue() {
             switch (ip) {
                 case 0:
                     if (cursor == null) {
@@ -94,7 +91,7 @@ namespace Sprixel {
                     // if skipto, skip some frames.  Used to implement CATCH
                     // invisibility
                     if (resultSlot != null) {
-                        cursor = (FrameBase)resultSlot;
+                        cursor = (Frame)resultSlot;
                         goto case 0;
                     }
                     ip = 2;
@@ -112,12 +109,13 @@ namespace Sprixel {
     }
 
     // This is quite similar to DynFrame and I wonder if I can unify them.
+    // These are always hashy for the same reason as Frame above
     public class DynObject: IPerl6Object {
-        public Dictionary<string, Variable> slots;
+        public Dictionary<string, object> slots;
         public Dictionary<string, IPerl6Object> methods;
         public IPerl6Object how;
 
-        public FrameBase InvokeMethod(FrameBase caller, string name,
+        public Frame InvokeMethod(Frame caller, string name,
                 LValue pos[], Dictionary<string, LValue> named) {
             IPerl6Object m = methods[name];
             if (m != null) {
@@ -129,7 +127,7 @@ namespace Sprixel {
             }
         }
 
-        public FrameBase Invoke(FrameBase caller, LValue pos[],
+        public Frame Invoke(Frame caller, LValue pos[],
                 Dictionary<string, LValue> named) {
             IPerl6Object d = slots["clr-delegate"];
             if (d != null) {
@@ -142,18 +140,29 @@ namespace Sprixel {
             }
         }
 
-        public FrameBase GetAttribute(FrameBase caller, string name) {
+        public Frame GetAttribute(Frame caller, string name) {
             caller.resultSlot = slots[name];
             return caller;
         }
 
-        public FrameBase HOW(FrameBase caller) {
+        public Frame HOW(Frame caller) {
             caller.resultSlot = how;
             return caller;
         }
 
-        public FrameBase WHICH(FrameBase caller) {
+        public Frame WHICH(Frame caller) {
             /* return a proxy for the Object which uses referential equality */
         }
+    }
+
+    // A bunch of stuff which raises big circularity issues if done in the
+    // setting itself.
+    // Provides: ClassHOW, ClassHOW.HOW, ClassHOW.add_method, ScalarContainer,
+    // ScalarContainer.HOW, Code, Code.HOW, Body, Body.HOW, Scope, Scope.HOW,
+    // ...
+    // This should be enough to implement the rest of ClassHOW :)
+    public class KernelSetting {
+        public static readonly IPerl6Object KernelFrame;
+        public static readonly IPerl6Object KernelScope;
     }
 }
