b1b9752d41d8a397a6f4aac6fedbbe6f3a360818
Stefan O'Rear <stefanor@cox.net>
2010-08-05 22:05:54 -0700

    start prototyping Array accessors

--- test2.pl
+++ test2.pl
@@ -5,11 +5,25 @@ my class Array is List {
     method new() {
         Array.RAWCREATE("flat", 1, "items", LLArray.new, "rest", LLArray.new);
     }
+
+    method push(*@x) {
+        $!rest.push(@x);
+    }
+
+    method Numeric { self.elems }
 }
 PRE-INIT { Q:CgOp { (prog (rawsset Kernel.ArrayP (@ (l Array))) (null Variable)) } }
+
+sub prefix:<+>($x) { $x.Numeric }
 
 my @x;
 
 ok @x ~~ Array, '@x isa Array';
+ok @x.elems == 0, 'no elements';
+ok +@x == 0, 'no elements (+)';
+
+@x.push(5);
+ok @x.elems == 1, 'one element now';
+ok @x.shift == 5, 'element removed is 5';
 
 done-testing;
