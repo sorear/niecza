0d043ebd16860fa05d5151f2eed50790b9d5eef7
Stefan O'Rear <stefanor@cox.net>
2010-09-17 13:49:36 -0700

    Fold next,redo,last,return back into setting

--- test.pl
+++ test.pl
@@ -2,7 +2,7 @@
 
 use Test;
 
-plan 517;
+plan 541;
 
 ok 1, "one is true";
 ok 2, "two is also true";
@@ -1072,4 +1072,34 @@ rxtest /y [ [a||b] | c ]: y/, "|| exposes a declarative prefix",
 {
     ok "foo" ~~ / :my $gothere = 1; foo /, "can embed :my in regexes";
     ok $gothere, ":my code is run";
+}
+
+{
+    sub flow-ok($fn, $flw, $msg) {
+        my $log = '';
+        $fn(-> $i { $log ~= $i });
+        is $log, $flw, $msg;
+    }
+
+    flow-ok -> &l { my $i = 0; while $i < 2 { $i++; l(1); next; l(2) } }, '11',
+        "next skips second half of while loop";
+    flow-ok -> &l { my $i = 0; while $i < 2 { $i++; l(1); last; l(2) } }, '1',
+        "last skips everything";
+    flow-ok -> &l { my $i = 0; while True { l($i++); last if $i == 3 } }, '012',
+        "last can leave inf loop";
+    flow-ok -> &l { my $i = 3; while $i == 3 { l($i--); redo if $i } }, '321',
+        "redo reenters loops";
+    sub foo { return 2; }
+    is foo(), 2, "return values work";
+    my $cont = False;
+    sub foo3 { return 2; $cont = True; }
+    foo3;
+    ok !$cont, "return exits function";
+}
+
+{
+    rxtest /^ x**2..4 $/, 'x**2..4', ('xx','xxx','xxxx'), ('x','xxxxx');
+    rxtest /^ x**2..* $/, 'x**2..*', ('xx','xxx','xxxx'), ('x',);
+    rxtest /^ [x**2] $/, 'x**2', ('xx',), ('x','xxx');
+    rxtest /^ [x**y] $/, 'x**y', ('x','xyx','xyxyx'), ('','xy','yx');
 }
