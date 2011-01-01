9697d6c6a3908b3002c165b0b740ee01832862c0
pmurias <pawelmurias@gmail.com>
2011-01-01 16:49:34 +0100

    [cl-backend] nam is turned into lisp and we use macros to handle the opcodes changed the test file to say "Hello World"

--- cl-backend/backend.lisp
+++ cl-backend/backend.lisp
@@ -4,6 +4,34 @@
 (ql:quickload "cl-json")
 (ql:quickload "fare-matcher")
 
+(defun to-symbol (string) (intern (string-upcase (concatenate 'string "nam-" string))))
+
+(defun first-string-to-symbol (thing)
+  (if (and thing (listp thing))
+    (cons (to-symbol (first thing)) (mapcar #'first-string-to-symbol (rest thing)))
+    thing
+  )
+)
+
+;(defmacro nam-sub (name body) `(defun ,(to-symbol name) () body))
+; for easier development
+(defmacro nam-sub (name body) body)
+
+(defmacro nam-ann (filename line op) op)
+(defmacro nam-prog (&body ops) `(progn ,@ops))
+(defun nam-sink (argument) nil)
+
+; TODO
+(defun nam-scopedlex (var) nil)
+(defun nam-scopedlex (var) nil)
+(defun nam-subcall (thing &rest args) (format t "calling sub~%"))
+(defun nam-fetch (thing) nil)
+(defun nam-const (thing) nil)
+(defun nam-double (number) nil)
+(defun nam-str (string) nil)
+(defun nam-box (type thing) nil)
+(defun nam-box (type thing) nil)
+
 (defun compile-sub (sub)
   (fare-matcher:match sub 
     ((list
@@ -25,7 +53,7 @@
         signature-params
         lexicals
         nam
-    ) (list 'this-is-a-sub nam))
+    ) (list sub name nam))
  )
 )
 
@@ -34,4 +62,10 @@
     ((list mainline-xref name log setting bottom-ref filename modtime x-ref t-deps root-stash) (mapcar #'compile-sub x-ref))
   )
 )
-(format t "~a" (compile-unit (json:decode-json (open "test.nam"))))
+(let (
+      (compiled-unit (first-string-to-symbol (first (compile-unit (json:decode-json (open "test.nam"))))))
+    )
+    (format t "~a~%" compiled-unit)
+;    (format t "expanded: ~a~%" (macroexpand compiled-unit))
+    (eval compiled-unit)
+)
