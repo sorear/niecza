39ddf6a041fde564d64968692bf41d27a7b19ef4
pmurias <pawelmurias@gmail.com>
2011-04-05 14:44:53 +0200

    [hoopl] parse more unit attributes

--- hoopl/nam.hs
+++ hoopl/nam.hs
@@ -12,36 +12,46 @@ data Xref = Xref {
     unit  :: String,  --  Names unit of origin
     index :: Integer, --  Indexes into unit's xref array
     xref_name  :: String  --  Descriptive name for debugging
-}
+} deriving Show
+
 data Unit = Unit {
---    mainline_ref ::   Xref,    -- Xref to mainline subroutine
-      name         ::   String  -- Unit's unique name
+      mainline_ref ::   Xref,    -- Xref to mainline subroutine
+      name         ::   String,  -- Unit's unique name
 --    log          ::   ...     -- Mostly unused vestige of last stash system
 --    setting      ::   String,  -- Name of setting unit or null
---    bottom_ref   ::   Xref,    -- Xref to sub containing {YOU_ARE_HERE}, or null
---    filename     ::   String,  -- Filename of source code or null
---    modtime      ::   Integer -- Seconds since 1970-01-01
+--    bottom_ref   ::   Xref    -- Xref to sub containing {YOU_ARE_HERE}, or null
+    filename     ::   String,  -- Filename of source code or null
+    modtime      ::   Integer -- Seconds since 1970-01-01
 --    xref         ::   [Xref]  -- Resolves refs from other units
 --    tdeps        ::   TDep    -- Holds dependency data for recompilation
 --    stash_root   ::   StNode  -- Trie holding classes and global variables
 } deriving Show
+ 
+instance FromJSON Xref where 
+    parseJSON (Array a) = do
+        unit  <- parseJSON (a ! 0)
+        index <- parseJSON (a ! 1)
+        xref_name <- parseJSON (a ! 2)
+        return $ Xref { unit=unit, index=index, xref_name=xref_name }
+        
+    parseJSON _ = mzero
+
 instance FromJSON Unit where 
     parseJSON (Array a) = do
+        mainline_ref <- parseJSON (a ! 0)
         name <- parseJSON (a ! 1)
-        return $ Unit {name=name}
---    parseJSON (Array a) = let [
---                                mainline_ref,
---                               name        ,
---                               log         ,
---                               setting     ,
---                               bottom_ref  ,
---                               filename    ,
---                               modtime     ,
---                               xref        ,
---                               tdeps       ,
---                               stash_root
---                               ] = toList a in return $ Unit mainline_ref setting bottom_ref filename modtime
---    parseJSON _ = mzero
+        -- skipping log and setting 
+--        bottom_ref <- parseJSON (a ! 4)
+        filename <- parseJSON (a ! 5)
+        modtime <- parseJSON (a ! 6)
+        return $ Unit {
+            mainline_ref=mainline_ref,
+            name=name,
+--            bottom_ref=bottom_ref
+            filename=filename,
+            modtime=modtime
+        }
+    parseJSON _ = mzero
 main = do
     nam <- (B.readFile "MAIN.nam")
     let (Done rest r) = parse json nam
