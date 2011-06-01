e2a35a1403473e6ffcac193e2be9c9faa0e39cb1
Stefan O'Rear <stefanor@cox.net>
2011-06-01 13:32:43 -0700

    FatRat.new, FatRat abs tests, bind numerator/denominator to Perl

--- test2.pl
+++ test2.pl
@@ -114,6 +114,10 @@ ok (10/2).abs == 10/2, "(10/2).abs == 10/2";
 ok (-10/2).abs == 10/2, "(-10/2).abs == 10/2";
 is (-10/2).abs.WHAT, (10/2).WHAT, "(-10/2).abs and 10/2 have same WHAT";
 
+ok FatRat.new(10,2).abs == FatRat.new(10,2), "(10/2).abs == 10/2";
+ok FatRat.new(-10,2).abs == FatRat.new(10,2), "(-10/2).abs == 10/2";
+is FatRat.new(-10,2).abs.WHAT, FatRat, "FatRat.new(-10,2).abs.WHAT";
+
 ok (10.Num).abs == 10.Num, "(10.Num).abs == 10.Num";
 ok (-10.Num).abs == 10.Num, "(-10.Num).abs == 10.Num";
 is (-10.Num).abs.WHAT, (10.Num).WHAT, "(-10.Num).abs and 10.Num have same WHAT";
@@ -124,6 +128,12 @@ is (-10 - 5i).abs.WHAT, "Num()", "(-10 - 5i).abs is a Num";
 
 ok (10 + 5i).re == 10, "(10 + 5i).re == 10";
 ok (10 + 5i).im == 5, "(10 + 5i).im == 5";
+
+ok (10/3).numerator == 10, "(10/3).numerator == 10";
+ok (10/3).denominator == 3, "(10/3).denominator == 3";
+is FatRat.new(10,3).WHAT, FatRat, "FatRat.new returns FatRat";
+ok FatRat.new(10,3).numerator == 10, "FatRat.new(10,3).numerator == 10";
+ok FatRat.new(10,3).denominator == 3, "FatRat.new(10,3).denominator == 3";
 
 #is $?FILE, 'test.pl', '$?FILE works';
 #is $?ORIG.substr(0,5), '# vim', '$?ORIG works';
