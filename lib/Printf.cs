a89a9a623c127cb2804486a770c13bef32c56e42
diakopter <diakopter@gmail.com>
2011-08-27 12:17:42 -0700

    add Printf.cs

---
+++ lib/Printf.cs
@@ -0,0 +1,13 @@
+using Niecza;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+
+public partial class Builtins {
+
+    public static Variable sprintf(Variable[] args) {
+        return args[0];
+    }
+}
