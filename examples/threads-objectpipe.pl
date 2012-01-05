7896533c16c6ebd0c8435e18c8be57e4261b5ffe
Daniel Ruoso <daniel@ruoso.com>
2012-01-04 23:50:22 -0500

    make the example more explicit
    The semantics of batch-processing imply that we may actually not use all the data
    that we produce. But we dont wait for each value to be used to produce another one
    The output of the example shows that the thread will produce a set of objects
    before the context switches again.

--- examples/threads-objectpipe.pl
+++ examples/threads-objectpipe.pl
@@ -1,8 +1,9 @@
 use Threads;
 my ($read, $write) = objectpipe;
 my $t = Threads::Thread.new({
-    for 1..200 -> $i { $write.put($i~'-'~time); };
-    CATCH { default { say "pipe closed..." } }
+    my $last = 0;
+    for 1..200 -> $i { $write.put($i~'-'~time); $last = $i; };
+    CATCH { default { say "pipe closed... sent $last items" } }
 });
 {
     for 1..5 {
@@ -13,3 +14,4 @@ my $t = Threads::Thread.new({
     $read = Nil;
 }
 $t.join();
+say "compare the number of the last consumed item, with the last sent item."
